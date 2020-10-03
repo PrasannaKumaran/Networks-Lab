c8be80fd0a48d2200c4bbbdd40f55f0f0f42e7eb
PrasannaKumaran <prasannakumaran176@gmail.com>
2020-10-03 19:32:07 +0530

    Client Server communication using TCP

---
+++ Assignment2/client_v2.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<string.h>
+
+int main(int argc,char **argv)
+{
+
+	int len;
+	int sockfd,n;
+	struct sockaddr_in servaddr,cliaddr;
+	
+	char str[1000]; char buff[1024];
+	
+	sockfd=socket(AF_INET,SOCK_STREAM,0); 
+	if(sockfd<0)
+		perror("cannot create socket");
+	
+	bzero(&servaddr,sizeof(servaddr));
+	
+	cliaddr.sin_family=AF_INET;
+	cliaddr.sin_addr.s_addr=inet_addr("127.0.0.1");
+	cliaddr.sin_port=htons(8081);
+	
+	connect(sockfd,(struct sockaddr*)&cliaddr,sizeof(cliaddr));
+	
+	//Sending Message
+	printf("Enter message to be sent:(End with !)");
+	scanf("%[^!]s",buff);
+	printf("\nClient:%s",buff);
+	n=write(sockfd,buff,sizeof(buff));
+	n=read(sockfd,buff,sizeof(buff));
+	printf("\nMessage Echoed from Server:%s\n",buff);
+	close(sockfd);
+	return 0;
+}
+
+
+
